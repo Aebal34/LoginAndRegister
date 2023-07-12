fd904d8bec7bb3c240ce88f21f152805b97614cf
Joren <97386268+Aebal34@users.noreply.github.com>
2023-07-12 12:42:27 +0200

    Project Start

---
+++ src/test/java/com/Utopia/Joren/LoginAndRegister/LoginAndRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Utopia.Joren.LoginAndRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
